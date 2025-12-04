e303d281f3483d505b73dead9a0fca3526838ce0
piero12004 <pierobajonero.12004@gmail.com>
2025-12-04 17:26:54 -0500

    prueba multiple proceso

--- Backend/src/models/Candidato.js
+++ Backend/src/models/Candidato.js
@@ -6,9 +6,13 @@ const candidatoSchema = new mongoose.Schema({
   cargo: { type: String, required: true },
   estado: { type: String, enum: ["activo", "inactivo"], default: "activo" },
   foto: { type: String },
-  proceso: { type: mongoose.Schema.Types.ObjectId, ref: "ProcesoElectoral", required: function() {
-    return this.estado === "activo";
-  }}
+  procesos: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "ProcesoElectoral" }],
+    required: function() {
+      return this.estado === "activo";
+    },
+    default: []
+  }
 });
 
 export default mongoose.model("Candidato", candidatoSchema);
