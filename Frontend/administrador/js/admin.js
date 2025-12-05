df7b6b6fd9d5e700e6038cb586dd843000d0c722
piero12004 <pierobajonero.12004@gmail.com>
2025-12-05 01:19:34 -0500

    fechas y periodos listo

--- Frontend/administrador/js/admin.js
+++ Frontend/administrador/js/admin.js
@@ -9,3 +9,17 @@ export function cargarNombreAdmin() {
     const titulo = document.getElementById("tituloAdmin");
     if (titulo) titulo.textContent = `Bienvenido ${nombre}`;
 }
+
+export function cerrarSesion() {
+    localStorage.removeItem("tokenAdmin");
+    localStorage.removeItem("adminUsuario");
+    window.location.href = "login.html";
+}
+
+// Función para proteger páginas: redirige al login si no hay token
+export function protegerPagina() {
+    const token = localStorage.getItem("tokenAdmin");
+    if (!token) {
+        window.location.href = "login.html";
+    }
+}
