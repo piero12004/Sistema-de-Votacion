541e95e4f20ec59d0126d64711256dfffe1c9d61
Guillermo2024-UTP <guillermosolidoro@gmail.com>
2025-12-04 20:32:25 -0500

    Segundo avance de las funcionalidades del usuario

--- Backend/src/routes/usuario.routes.js
+++ Backend/src/routes/usuario.routes.js
@@ -1,18 +1,13 @@
 import { Router } from "express";
-import { 
-    registerUsuario, 
-    loginUsuario, 
-    getProcesosActivos, 
-    getCandidatosPorProceso 
-} from "../controllers/usuario.controller.js";
+import { registerUsuario, loginUsuario, getProcesosActivos, getCandidatosPorProceso,emitirVoto } from "../controllers/usuario.controller.js";
 
 import protect from "../middlewares/authUsuario.js"; 
 
 const router = Router();
-
 router.post("/register", registerUsuario);
 router.post("/login", loginUsuario);
-router.get("/procesos/activos", protect, getProcesosActivos);
-router.get("/proceso/:idProceso/candidatos", protect, getCandidatosPorProceso);
+router.get("/procesos-activos", protect, getProcesosActivos); 
+router.get("/:idProceso/candidatos", protect, getCandidatosPorProceso); 
+router.post("/votar", protect, emitirVoto); 
 
 export default router;
